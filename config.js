c825adb670cd8c08c01e34a2b04cd484f9adbb23
zzqqser <zzqq888999@gmail.com>
2025-01-07 15:35:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2671488-fb491a80091166a48bc66b17',
+    'ur2779633-6a22308ed8fbe0c59fa5ee12',
     //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
     //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
     //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
@@ -22,15 +22,15 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://uptime.001315.xyz/'
+      url: ''
     },    
     {
       text: 'GitHub',
-      url: 'https://github.com/shaoyouvip/uptime/'
+      url: ''
     },
     {
       text: '博客',
-      url: 'https://blog.aizrf.COM/'
+      url: ''
     },
   ],
 };
